10d29cd07eccc0d0d01d1d7157c64d3aca88682e
coreschke <coreschke@mac.com>
2022-09-05 20:53:05 -0400

    Pagination done, changed layout to a grid

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,50 +1,108 @@
 import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Thumbnail from "./components/Thumbnail";
+import Pagination from 'react-bootstrap/Pagination';
 
 function App() {
-const [allPokemons, setAllPokemons] = useState([]);
-const [loadPoke, setLoadPoke] = useState(
-	"https://pokeapi.co/api/v2/pokemon?limit=25&offset=0"
-);
-// const [currentPage, setCurrentPage] = useState(1);
+	const [allPokemons, setAllPokemons] = useState([]);
+	const [previousPoke, setPreviousPoke] = useState(
+		"https://pokeapi.co/api/v2/pokemon?limit=25&offset=0"
+	);
+	const [nextPoke, setNextPoke] = useState(
+		"https://pokeapi.co/api/v2/pokemon?limit=25&offset=0"
+	);
+	const [pageLimit, setPageLimit] = useState(10);
+  	const [currentPage, setCurrentPage] = useState(1);
+	const [uniqueIds, setUniqueIds] = useState([]);
 
-const getAllPokemons = async () => {
-	const res = await fetch(loadPoke);
-	const data = await res.json();
-  console.log(data);
-	setLoadPoke(data.next);
-
-	function createPokemonObject(result) {
-	result.forEach(async (pokemon) => {
-		const res = await fetch(
-		`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
-		);
-    // console.log(pokemon.name);
+	// Load the next 25 pokemon, confirm no duplicates and sort by id. Set next API endpoints for either direction for pagination
+	const getNextPokemons = async () => {
+		const res = await fetch(nextPoke);
 		const data = await res.json();
-		setAllPokemons((currentList) => [...currentList, data]);
-	});
+		setNextPoke(data.next);
+		setPreviousPoke(data.previous);
+		setPageLimit(pageLimit => Math.ceil(data.count / 25));
+
+		function createPokemonObject(result) {
+			result.forEach(async (pokemon) => {
+				const res = await fetch(
+				`${pokemon.url}`
+				);
+				const data = await res.json();
+				if (!uniqueIds.includes(data.id)) {
+					setAllPokemons((currentList) => [...currentList, data]);
+					uniqueIds.push(data.id);
+				}
+			});
+		}
+		createPokemonObject(data.results);
+		allPokemons.sort((a, b) => a.id - b.id);
+	};
+
+	// Load the previous 25 pokemon, confirm no duplicates and sort by id. Set next API endpoints for either direction for pagination
+	const getPreviousPokemons = async () => {
+		const res = await fetch(previousPoke);
+		const data = await res.json();
+		setPageLimit(pageLimit => Math.ceil(data.count / 25));
+		setNextPoke(data.next);
+		setPreviousPoke(data.previous);
+
+		function createPokemonObject(result) {
+			result.forEach(async (pokemon) => {
+				const res = await fetch(
+				`${pokemon.url}`
+				);
+				const data = await res.json();
+				if (!uniqueIds.includes(data.id)) {
+					setAllPokemons((currentList) => [...currentList, data]);
+					uniqueIds.push(data.id);
+				}
+			});
+		}
+		createPokemonObject(data.results);
+		allPokemons.sort((a, b) => a.id - b.id);
+	};
+
+	// Clear pokemon array, call function to load pokemon, update current page, and clear unique ids array
+	const prevClick = () => {
+		setAllPokemons(allPokemons => []);
+		getPreviousPokemons();
+		setCurrentPage(currentPage => currentPage - 1);
+		setUniqueIds(uniqueIds => []);
 	}
-	createPokemonObject(data.results);
-};
-useEffect(() => {
-	getAllPokemons();
-}, []);
+
+	// Clear pokemon array, call function to load pokemon, update current page, and clear unique ids array
+	const nextClick = () => {
+		setAllPokemons(allPokemons => []);
+		getNextPokemons();
+		setCurrentPage(currentPage => currentPage + 1);
+		setUniqueIds(uniqueIds => []);
+	}
+
+	useEffect(() => {
+		setAllPokemons(allPokemons => []);
+		getNextPokemons();
+	}, []);
 
 return (
 	<div className="pokemon-container">
 		<div className="all-container">
 		{allPokemons.map((pokemon) => (
 			<Thumbnail
-        name={pokemon.name}
-        image={pokemon.sprites.other.dream_world.front_default}
-        key={pokemon.index}
-        types={pokemon.types}
-        weight={pokemon.weight}
-        stats={pokemon.stats}
+				name={pokemon.name}
+				image={pokemon.sprites.other.dream_world.front_default}
+				key={pokemon.index}
+				types={pokemon.types}
+				weight={pokemon.weight}
+				stats={pokemon.stats}
 			/>
 		))}
 		</div>
+		<Pagination>
+			<Pagination.Prev disabled={currentPage === 1} onClick={prevClick}/>
+			<Pagination.Item active>{currentPage}</Pagination.Item>
+			<Pagination.Next disabled={currentPage === pageLimit} onClick={nextClick}/>
+		</Pagination>
 	</div>
 );
 }
--- pokemon-app/src/components/Thumbnail.js
+++ pokemon-app/src/components/Thumbnail.js
@@ -18,49 +18,52 @@ const [show, setShow] = useState(false);
 const handleClose = () => setShow(false);
 const handleShow = () => setShow(true);
 return (
+	// Style of pokemon "card" is the color of its primary type
 	<div className={style}>	
-
-	<div className="detail-wrapper">
-		<h3 onClick={handleShow}>{name.toUpperCase()}</h3>
-		<Modal 
-		show={show}
-		onHide={handleClose}
-		size="lg"
-		aria-labelledby="contained-modal-title-vcenter"
-		centered>
-			<Modal.Header closeButton className = {types[0].type.name}>
-				<Modal.Title>{name.toUpperCase()}</Modal.Title>
-			</Modal.Header>
-			<Modal.Body className = {types[0].type.name}>
-				<Card>
-					<Card.Img variant="top" src={image} style={{ width: '16rem' }} class="mx-auto d-block"/>
-					<Card.Body>
-						<Card.Text class="text-center">
-						Weight: {weight} lbs
-						</Card.Text>
-					</Card.Body>
-					
-					<ListGroup as="ol" numbered>
-					<div className="fw-bold">Pokemon Types</div>
-					{types.map((i)=> 
-						<ListGroup.Item as="li">{i.type.name}</ListGroup.Item>)
-					}
-					</ListGroup>
-					<ListGroup variant="flush">
-					<div className="fw-bold">Pokemon Stats</div>
-					{stats.map((i)=> 
-						<ListGroup.Item>{i.stat.name} is {i.base_stat}</ListGroup.Item>)
-					}
-					</ListGroup>
-				</Card>
-			</Modal.Body>
-			<Modal.Footer>
-				<Button variant="secondary" onClick={handleClose}>
-					Close
-				</Button>
-			</Modal.Footer>
-		</Modal>
-	</div>
+		<div className="detail-wrapper">
+			<h3 onClick={handleShow}>{name.toUpperCase()}</h3>
+			{/* Upon click of pokemon name, modal pops up with photo and additional information */}
+			<Modal 
+			show={show}
+			onHide={handleClose}
+			size="lg"
+			aria-labelledby="contained-modal-title-vcenter"
+			centered>
+				<Modal.Header closeButton className = {types[0].type.name}>
+					<Modal.Title>{name.toUpperCase()}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body className = {types[0].type.name}>
+					<Card>
+						<Card.Img variant="top" src={image} style={{ width: '16rem' }} className="mx-auto d-block"/>
+						<Card.Body>
+							<Card.Text className="text-center">
+							Weight: {weight} lbs
+							</Card.Text>
+						</Card.Body>
+						
+						<ListGroup as="ol" numbered>
+						<div className="fw-bold">Pokemon Types</div>
+						{/* Iterate through types for each pokemon */}
+						{types.map((i)=> 
+							<ListGroup.Item as="li">{i.type.name}</ListGroup.Item>)
+						}
+						</ListGroup>
+						<ListGroup variant="flush">
+						<div className="fw-bold">Pokemon Stats</div>
+						{/* Iterate through stats for each pokemon */}
+						{stats.map((i)=> 
+							<ListGroup.Item>{i.stat.name} is {i.base_stat}</ListGroup.Item>)
+						}
+						</ListGroup>
+					</Card>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						Close
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</div>
 	</div>
 );
 };
